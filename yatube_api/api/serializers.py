1032f00e8e6255be613a881e8d8217f951060762
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-03-06 14:22:41 +0300

    first review

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from posts.models import Comment, Group, Post
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = (
+            'id',
+            'title',
+            'slug',
+            'description',
+        )
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        slug_field="username",
+        read_only=True,
+    )
+
+    class Meta:
+        fields = (
+            'id',
+            'text',
+            'pub_date',
+            'author',
+            'image',
+            'group',
+        )
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = (
+            'id',
+            'author',
+            'post',
+            'text',
+            'created',
+        )
+        model = Comment
---
+++ yatube_api/api/views.py
@@ -0,0 +1,38 @@
+from django.db.models.query import QuerySet
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+
+from posts.models import Group, Post
+
+from .permissions import OrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (OrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer: PostSerializer) -> None:
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = (OrReadOnly,)
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer: CommentSerializer) -> None:
+        post_id = self.kwargs.get("post_id")
+        post = get_object_or_404(Post, pk=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+    def get_queryset(self) -> QuerySet:
+        post_id = self.kwargs.get("post_id")
+        post = get_object_or_404(Post, pk=post_id)
+        comments_queryset = post.comments.all()
+        return comments_queryset
